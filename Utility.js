2f6e01cc54f0c6972108d526bd9300fa4b952380
nitinmakin <nitinmakin124.com>
2020-09-26 21:44:56 +0530

    Added-(UC7)-Two players can  play the game

--- Utility.js
+++ Utility.js
@@ -1,66 +1,112 @@
-let StartingPosition = 0;
+let PlayerOneStartingPosition = 0;
+let PlayerTwoStartingPositin = 0
 const WinningPosition = 100;
-let count = 0;
+let PlayerOneCount = 0;
+let PlayerTwoCount = 0;
 let throwDice;
 let option;
+let turn = 0;
 
-class SnakeLadder {
-
+class SnakeLadder {   
     getRandomNo = (input) => {
         return (Math.floor(Math.random() * input)) + 1;
     }
 
-    getWinResult = () => {
-        while (StartingPosition != WinningPosition) {
-            throwDice = this.getRandomNo(6);
-            option = this.getRandomNo(3);
-            switch (option) {
-                case 1:
-                    StartingPosition = this.getSnakeMove();
-                    count++;
-
-                case 2:
-                    this.getFoulMove();
-                    count++;
-
-                case 3:
-                    StartingPosition = this.getLadderMove();
-                    count++;
-            }
-        }
-        console.log("TOTAL NO OF DICE PLAYED==>" + count)
-    }
-
-    getSnakeMove = () => {
+    getSnakeMove = (position) => {
         console.log("YOU GOT==>" + throwDice)
         console.log("OOPS.. YOU STEP ON SNAKE")
-        StartingPosition = StartingPosition - throwDice;
-        if (StartingPosition < 0)
-            StartingPosition = StartingPosition + throwDice;
-        console.log("YOUR CURRENT POSITION==>" + StartingPosition)
+        position = position - throwDice;
+        if (position < 0)
+        position = position + throwDice;
+        console.log("YOUR CURRENT POSITION==>" + position)
         console.log();
-        return StartingPosition;
+        return position;
     }
 
-    getFoulMove = () => {
+    getFoulMove = (position) => {
         console.log("YOU GOT==>" + throwDice)
         console.log("OOPS...YOU MADE A FOUL")
         console.log("YOU WILL BE IN SAME POSITION")
+        console.log("YOUR CURRENT POSITION IS "+position)
         console.log();
+        return position;
     }
 
-    getLadderMove = () => {
+    getLadderMove = (position) => {
         console.log("YOU GOT==>" + throwDice)
         console.log("CONGRATS... YOU STEP ON LADDER")
-        StartingPosition = StartingPosition + throwDice;
-        if (StartingPosition > WinningPosition) {
-            StartingPosition = StartingPosition - throwDice;
-            console.log("OOPS...YOU CANT EXCEED 100")
+        position = position + throwDice;
+        if (position > WinningPosition) {
+            position = position - throwDice;
+            console.log("OOPS...YOU CANT EXCEED "+WinningPosition)
         }
-        console.log("YOUR CURRENT POSITION IS " + StartingPosition)
+        console.log("YOUR CURRENT POSITION IS " + position)
         console.log();
-        return StartingPosition;
+        return position;
     }
+
+    getDoublePlayerResult = () => {
+
+        while (PlayerOneStartingPosition != WinningPosition && PlayerTwoStartingPositin != WinningPosition) {
+            throwDice = this.getRandomNo(6);
+            option = this.getRandomNo(3);            
+            if (turn % 2 == 0) {
+                turn++;
+                console.log("PLAYER 1 TURN ")
+                switch (option) {
+                    case 1:
+                        PlayerOneStartingPosition = this.getSnakeMove(PlayerOneStartingPosition);
+                        PlayerOneCount++;                     
+                        break;
+
+                    case 2:                       
+                        PlayerOneStartingPosition = this.getFoulMove(PlayerOneStartingPosition);
+                        PlayerOneCount++;                     
+                        break;
+
+                    case 3:                      
+                        PlayerOneStartingPosition = this.getLadderMove(PlayerOneStartingPosition);
+                        PlayerOneCount++;                       
+                        break;
+                }
+
+                if(PlayerOneStartingPosition == WinningPosition)
+                console.log("CONGRATS...PLAYER 1 WON THE GAME")
+            }
+            else {
+                turn++;
+                console.log("PLAYER 2 TURN ")
+                switch (option) {
+                    case 1:                       
+                       PlayerTwoStartingPositin = this.getSnakeMove(PlayerTwoStartingPositin);
+                        PlayerTwoCount++;                        
+                        break;
+
+                    case 2:                        
+                        PlayerTwoStartingPositin = this.getFoulMove(PlayerTwoStartingPositin);
+                        PlayerTwoCount++;                      
+                        break;
+                    case 3:                      
+                        PlayerTwoStartingPositin = this.getLadderMove(PlayerTwoStartingPositin);
+                        PlayerTwoCount++;                     
+                        break;
+                }
+                if(PlayerTwoStartingPositin == WinningPosition)
+                console.log("CONGRATS...PLAYER 2 WON THE GAME")
+            }
+        }
+        console.log("TOTAL NO OF DICE PLAYED BY PLAYER 1 ==>" + PlayerOneCount);
+        console.log("TOTAL NO OF DICE PLAYED BY PLAYER 2 ==>" + PlayerTwoCount)
+    }
+
+
+
+
+   
+
+   
+
+   
 }
 
 
