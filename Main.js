f3bc9181e1b3e1db9c5e8ee9a3789cddd7c6283a
nitinmakin <nitinmakin124.com>
2020-09-26 11:48:46 +0530

    Added(UC2)-to get random number between 1-6

--- Main.js
+++ Main.js
@@ -1,1 +1,3 @@
 console.log("Hello...Welcome to Snake & Ladder Game");
+const result = require('./Utility'); 
+console.log(result.getDiceRandomNo());
--- Utility.js
+++ Utility.js
@@ -1,5 +1,8 @@
 class SnakeLadder{
-    SinglePlayer(){
+    getDiceRandomNo(){
         var startPosition = 0;
-    }
+        return (Math.floor(Math.random()*6)) + 1;
+      }
 }
+
+module.exports = new SnakeLadder();
