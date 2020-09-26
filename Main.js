c848b8e7d850e4626c71769d283a104c5478a468
nitinmakin <nitinmakin124.com>
2020-09-26 12:08:39 +0530

    Added(UC3)-Option for no play,ladder and snake

--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
 console.log("Hello...Welcome to Snake & Ladder Game");
 const result = require('./Utility'); 
 console.log(result.getDiceRandomNo());
+console.log(result.getOption());
--- Utility.js
+++ Utility.js
@@ -1,7 +1,9 @@
 class SnakeLadder{
     getDiceRandomNo(){
-        var startPosition = 0;
         return (Math.floor(Math.random()*6)) + 1;
+      }
+      getOption(){
+          return (Math.floor(Math.random() * 3)) +1;
       }
 }
 
