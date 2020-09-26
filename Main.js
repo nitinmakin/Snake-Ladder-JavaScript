3d1d3a8f8af4d2e933056f60a2b1e2fdfd8d3183
nitinmakin <nitinmakin124.com>
2020-09-26 13:59:33 +0530

    Added(UC4)-Repeat dice until player reaches 100

--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
 console.log("Hello...Welcome to Snake & Ladder Game");
 const result = require('./Utility'); 
 console.log(result.getRandomNo(3));
+result.getWinResult();
--- Utility.js
+++ Utility.js
@@ -1,7 +1,38 @@
-class SnakeLadder{
-    getRandomNo(input){
-        return (Math.floor(Math.random()*input)) + 1;
-      }
+let StartingPosition = 0;
+let WinningPosition = 100;
+class SnakeLadder {
+    getRandomNo = (input) => {
+        return (Math.floor(Math.random() * input)) + 1;
+    }
+
+    getWinResult = () => {
+        while (StartingPosition <= WinningPosition) {
+            let throwDice = this.getRandomNo(6);
+            let option = this.getRandomNo(3);
+            switch (option) {
+                case 1:
+                    console.log("YOU GOT==>" + throwDice)
+                    console.log("OOPS.. YOU STEP ON SNAKE")
+                    StartingPosition = StartingPosition - throwDice;
+                    console.log("YOUR CURRENT POSITION==>" + StartingPosition)
+                    console.log();
+
+                case 2:
+                    console.log("YOU GOT==>" + throwDice)
+                    console.log("OOPS...YOU MADE A FOUL")
+                    console.log("YOU WILL BE IN SAME POSITION")
+                    console.log();
+
+                case 3:
+                    console.log("YOU GOT==>" + throwDice)
+                    console.log("CONGRATS... YOU STEP ON LADDER")
+                    StartingPosition = StartingPosition + throwDice;
+                    console.log("YOUR CURRENT POSITION IS " + StartingPosition)
+                    console.log();
+            }
+        }
+    }
 }
+
 
 module.exports = new SnakeLadder();
